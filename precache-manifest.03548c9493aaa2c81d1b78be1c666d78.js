6d14906e16d44dc82d6d1869e79b06d87519c107
kleyson <kleyson@users.noreply.github.com>
2019-12-11 01:48:29 +0000

    Automated deployment: Wed Dec 11 01:48:29 UTC 2019 ee65ed1d743b2d34dfd7f9e3bfe1754945811f7b

---
+++ precache-manifest.03548c9493aaa2c81d1b78be1c666d78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "38d24caf092a1f21a08e",
+    "url": "/pres/static/js/runtime~main.38d24caf.js"
+  },
+  {
+    "revision": "de68cbe71bb55c1d8463",
+    "url": "/pres/static/js/main.de68cbe7.chunk.js"
+  },
+  {
+    "revision": "7f84696904865195bfab",
+    "url": "/pres/static/js/1.7f846969.chunk.js"
+  },
+  {
+    "revision": "cc5f81429b2f659106488afa46c8ba39",
+    "url": "/pres/index.html"
+  }
+];
